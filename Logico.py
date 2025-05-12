0d66e0bcfcff56f0fb7b540565936b4bdbe2aee0
Cintia Souza <cintiacpereirapereiraesouza@gmail.com>
2025-05-12 11:09:50 -0300

    Add files via upload

---
+++ Logico.py
@@ -0,0 +1,10 @@
+vetorDeLogico = [False] * 4
+
+vetorDeLogico[0] = True
+
+#Usamos F-strings para formatar a saída, inserido o
+#Valor da variável.
+print(f"Na posição 0, o valor é.: {vetorDeLogico[0]}")
+print(f"Na posição 1, o valor é.: {vetorDeLogico[1]}")
+print(f"Na posição 2, o valor é.: {vetorDeLogico[2]}")
+print(f"Na posição 3, o valor é.: {vetorDeLogico[3]}")
