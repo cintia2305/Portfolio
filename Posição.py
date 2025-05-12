cd8a85a41ac84884975705f0d6385c4d72f5658b
Cintia Souza <cintiacpereirapereiraesouza@gmail.com>
2025-05-12 10:38:31 -0300

    Add files via upload

---
+++ Posição.py
@@ -0,0 +1,24 @@
+#   posição lista;
+
+IDADE = [0] * 8
+
+IDADE[0] = "Joana de Souza"
+IDADE[1] = "Pedro Costa"
+IDADE[2] = "Jaqueline Almeida"
+IDADE[3] = "Sandro Ferreira"
+IDADE[4] = "Carlos Souto"
+IDADE[5] = "Nilson Moreira"
+IDADE[6] = "Alessandra Fernandes"
+IDADE[7] = "Alair Guimaraes"
+
+#print Imprime cada nome
+# da lista em uma linha separada.
+
+print(IDADE[0])
+print(IDADE[1])
+print(IDADE[2])
+print(IDADE[3])
+print(IDADE[4])
+print(IDADE[5])
+print(IDADE[6])
+print(IDADE[7])
---
+++ vetor.py
@@ -0,0 +1,13 @@
+Em Python , não precisamos declarar o tipo e tamanho fixo e do vetor antecipamente.
+# Cria uma lista  com 4 elementos, iniciazados com 0
+vetorDelInteiros = [0] = 4
+vetorDelInteiros[0] * 2
+#Usamos f-strings para formatar a saida de forma concisa,
+
+print(f" Na posição 0, o valor é.: {"vetorDelInteiros[0]}")
+
+vetorDelCaracter = [None] * 4
+
+vetorDelCaracter [0] = " CINTIA"
+
+print(f" Na posição 0, valor.: {"vetorDelCaracter[0]}")
